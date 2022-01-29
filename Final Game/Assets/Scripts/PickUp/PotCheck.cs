a13797aa4c66c6e9f5f7bf2940ddaff28a2c166b
brightontp <08tpenner@brightoncollege.net>
2022-01-29 10:33:30 +0000

    Update PotCheck.cs

--- Final Game/Assets/Scripts/PickUp/PotCheck.cs
+++ Final Game/Assets/Scripts/PickUp/PotCheck.cs
@@ -7,8 +7,7 @@ public class PotCheck : MonoBehaviour
     public Interaction script;
     private Animator animator, animator1;
     public GameObject otherObject, ladel;
-    public float rayDistance = 4f;
-    private RaycastHit hitInfo;
+
 
 
     private void Start()
@@ -17,30 +16,19 @@ public class PotCheck : MonoBehaviour
         animator = otherObject.GetComponent<Animator>();
         animator1 = ladel.GetComponent<Animator>();
 
-
-
-
     }
 
 
     private void FixedUpdate()
     {
-        Vector3 origin = transform.position;
-        Vector3 direction = transform.forward;
 
-
-        Debug.DrawRay(origin, direction * 2f, Color.red);  //debugger ray
-        Ray ray = new Ray(origin, direction);
         if (script.canFlipPan)
         {
-            if (Physics.Raycast(ray, out hitInfo, rayDistance))
-            {
-                if (Input.GetKeyDown("e"))
-                {
-                    animator.Play("Pot");  //plays pot fall animation if e is pressed and player is withing raydistance of wall
-                    animator1.Play("LadelFall");
-                }
-            }
+
+            animator.Play("Pot");  //plays pot fall animation if e is pressed and player is withing raydistance of wall
+            animator1.Play("LadelFall");
+               
+           
         }
 
 
