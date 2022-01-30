85cd85b7b85025b2574a0178b317e58a44c4bfe1
brightontp <08tpenner@brightoncollege.net>
2022-01-30 19:22:56 +0000

    .

---
+++ Final Game/Assets/Scripts/IInteractable 2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// inbetween script for linking scripts
+
+public interface IInteractable
+{
+    void Interact();
+}
